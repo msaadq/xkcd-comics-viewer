ede868aeb2d8366c1846f786f0e085b6344fe0f7
Saad Qureshi <msaadq94@gmail.com>
2020-03-15 13:17:02 +0100

    Initial Commit

---
+++ xkcd-comics-viewerTests/xkcd_comics_viewerTests.swift
@@ -0,0 +1,34 @@
+//
+//  xkcd_comics_viewerTests.swift
+//  xkcd-comics-viewerTests
+//
+//  Created by Saad Qureshi on 15/03/2020.
+//  Copyright © 2020 Saad. All rights reserved.
+//
+
+import XCTest
+@testable import xkcd_comics_viewer
+
+class xkcd_comics_viewerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
